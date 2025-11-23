94b906c319f3722f0837dddd2b91c23db9424e63
kimmo <hemakim0215@naver.com>
2025-11-23 10:41:31 +0900

    .

--- apps/rust/senti_scanner/src/net_packet.rs
+++ apps/rust/senti_scanner/src/net_packet.rs
@@ -1,3 +1,27 @@
 use crate::net_tcp;
 use crate::net_event_listner;
 
+#[repr(u8)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PacketType {
+    Connect,
+    Disconnect,
+    Data,
+}
+
+
+#[derive(Debug, Clone)]
+pub struct DataPacket {
+    pub packet_type: u16,
+    pub data: String,
+}
+
+impl DataPacket {
+    pub fn new_unassigned(_packet_type : u16, _data : String) -> Self {
+        Self { packet_type: _packet_type, data: _data }
+    }
+    pub fn validate(&self) -> Result<(), &'static str> {
+        if self.data.len() > u16::MAX as usize { return Err("message too long"); }
+        Ok(())
+    }
+}
